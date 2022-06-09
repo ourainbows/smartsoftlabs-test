dea3fff6717081af6bc3d09be18d02f78c92b702
ourainbows <casasjuanfelipe@gmail.com>
2022-06-08 23:09:17 -0500

    Created function to group data by province

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,30 @@ import * as fs from "fs";
 import * as path from "path";
 import { parse } from "csv-parse";
 
-type provinceState = {
+interface ProvinceState {
   name: string;
   population: number;
-  deaths: number;
+  accumulatedDeaths: number;
+};
+
+const groupedByProvince = (data: any): ProvinceState[] => {
+  const grouped: ProvinceState[] = data.reduce((acc: ProvinceState[], curr: any) => {
+    const province = curr.Province_State;
+    const population = curr.Population;
+    const deaths = curr["4/27/21"];
+    if (acc[province]) {
+      acc[province].population += population;
+      acc[province].accumulatedDeaths += deaths;
+    } else {
+      acc[province] = {
+        name: province,
+        population: population,
+        accumulatedDeaths: deaths,
+      };
+    }
+    return acc;
+  });
+  return grouped;
 };
 
 (() => {
@@ -14,7 +34,7 @@ type provinceState = {
     "assets/time_series_covid19_deaths_US.csv"
   );
 
-  const headers = ["Province_State", "Population", "deaths"];
+  //const headers = ["Province_State", "Population", "4/27/21"];
 
   const fileContent = fs.readFileSync(csvFilePath, { encoding: "utf-8" });
 
@@ -23,13 +43,24 @@ type provinceState = {
     {
       delimiter: ",",
       columns: true,
+      cast: (columnValue, context) => {
+        if (context.column === "Population") {
+          return parseInt(columnValue);
+        }
+        if (context.column === "4/27/21") {
+          return parseInt(columnValue);
+        }
+
+        return columnValue;
+      },
     },
     (error, result: any) => {
       if (error) {
         console.error(error);
       }
 
-      console.log("Result", result);
+      const grouped = groupedByProvince(result);
+      console.log(grouped);
     }
   );
 })();
